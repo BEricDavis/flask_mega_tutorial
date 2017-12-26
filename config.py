c68ba41608039eecdd712aa0852b9d6b6e388b0e
B Eric Davis <feralmonkey@gmail.com>
2017-12-26 10:07:46 -0500

    add comment to config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
 class Config(object):
+    # used to sign cookies etc.  Flask-WTF uses it to protect against CSRF
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
