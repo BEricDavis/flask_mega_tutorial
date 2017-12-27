472010e16057273d7dd790d82c0790a8d93b25df
B Eric Davis <feralmonkey@gmail.com>
2017-12-27 08:40:18 -0500

    Begin adding database classes and config

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # used to sign cookies etc.  Flask-WTF uses it to protect against CSRF
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
