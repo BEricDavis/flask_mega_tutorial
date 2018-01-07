36c2d136193711dd6561aaa4d77c5f1a9bcd7fb2
B Eric Davis <feralmonkey@gmail.com>
2018-01-06 20:07:18 -0500

    part 7: add emailing of errors

--- config.py
+++ config.py
@@ -4,6 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # used to sign cookies etc.  Flask-WTF uses it to protect against CSRF
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # SQLAlchemy parameters
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Email config for error handling
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['feralmonkey@gmail.com']
