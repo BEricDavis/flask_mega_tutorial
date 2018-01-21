0c5620f68d8c1d26e310c74ed9b277473d192e2e
B Eric Davis <feralmonkey@gmail.com>
2018-01-21 17:09:55 -0500

    part 9: Add pagination

--- config.py
+++ config.py
@@ -15,3 +15,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['feralmonkey@gmail.com']
+    # Pagination settings
+    POSTS_PER_PAGE = 3
