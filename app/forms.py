7c1674d42e998743383b780ddc474e03374d5517
B Eric Davis <feralmonkey@gmail.com>
2018-02-11 20:04:19 -0500

    part 10: initial changes to enable password reset

--- app/forms.py
+++ app/forms.py
@@ -47,3 +47,7 @@ class EditProfileForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
