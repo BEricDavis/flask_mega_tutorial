c16274b76de158e938bdef515a11c53e5db9b011
B Eric Davis <feralmonkey@gmail.com>
2018-03-17 14:20:15 -0400

    13.8 import cli

--- microblog.py
+++ microblog.py
@@ -1,7 +1,7 @@
-from app import app, db
+from app import app, db, cli
 from app.models import User, Post
 
-# create a shell context for the 'flask shell' command
+
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
